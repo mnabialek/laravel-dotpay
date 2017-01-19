39f1667ab39be331fb91216902f79db5286477af
Marcin Nabiałek <dev@nabialek.org>
2017-01-19 18:52:55 +0100

    Change type names

--- src/Options/Type.php
+++ src/Options/Type.php
@@ -5,8 +5,8 @@ namespace Mnabialek\LaravelDotpay\Options;
 class Type
 {
     const BACK_BUTTON = 0;
-    const CONFIRMATION_URL = 1;
+    const CALLBACK_URL = 1;
     const NONE = 2;
-    const BACK_BUTTON_AND_CONFIRMATION_URL = 3;
+    const BACK_BUTTON_AND_CALLBACK_URL = 3;
     const DIRECT = 4;
 }
